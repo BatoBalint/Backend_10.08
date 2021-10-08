6e33357048fdfb10972d9ad340ab6282478acea9
Bato Balint <balint.bato@gmail.com>
2021-10-08 15:09:05 +0200

    doublefuck

--- index.php
+++ index.php
@@ -1,9 +1,40 @@
 <?php
   class Allat {
+    private $nev;
+    private $fajta;
+    private $orokbefogadva;
 
+    public function getNev() {
+      return $this->nev;
+    }
+
+    public function setNev(string $ujNev) {
+      if(empty(trim($ujNev))) {
+        throw new Exception("A nev nem lehet ures.");
+      }
+      $this->nev = $ujNev;
+    }
+
+    public function getFajta() {
+      return $this->fajta;
+    }
+
+    public function getOrokbefogadva() {
+      return $orokbefogadva->format("c");
+    }
+
+    public function __construct(string $_nev, string $_faj) {
+      if (empty(trim($_nev))) {
+        throw new Exception("Nev nem lehet ures.");
+      }
+      $this->nev = $_nev;
+      if (empty(trim($_faj))) {
+        throw new Exception("Faj nem lehet ures.");
+      }
+      $this->fajta = $_faj;
+      $this->orokbefogadva = new DateTime();
+    }
   }
-
-  Allat a1 = new Allat();
 ?><!DOCTYPE html>
 <html lang="hu" dir="ltr">
   <head>
@@ -11,6 +42,11 @@
     <title></title>
   </head>
   <body>
-
+    <?php
+      $kutya = new Allat("Ferenc", "Vizsla");
+      echo $kutya->getNev();
+      echo $kutya->getFajta();
+      echo $kutya->getOrokbefogadva();
+    ?>
   </body>
 </html>
