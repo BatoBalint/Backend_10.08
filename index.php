47dcc2080b641c70a488a11febd7ffabdb299cff
Bato Balint <balint.bato@gmail.com>
2021-10-08 14:01:54 +0200

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  class Allat {
+
+  }
+
+  Allat a1 = new Allat();
+?><!DOCTYPE html>
+<html lang="hu" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
